e866960796544ecaa39b260f025410f3d4e007ed
pokefan56 <109863157+pokefan56@users.noreply.github.com>
2023-07-18 19:27:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ api_id = 9976721
 
 api_hash = "3ef17a8cdb938335bd8ba292e6d816aa"
 
-bot_token = "6094200729:AAF7U2jM_T35Wb4nWdvPdndFNjtZ7oQeTTA"
+bot_token = "6364807461:AAF7NgSElmipKP6fvRAzFIGNhWqvI-RDQ_8"
 
-GROUP_TAG = "RC"
+GROUP_TAG = "DC"
 
-sudo_users = [1606667548]
+sudo_users = []
 
 DL_DONE_MSG = """
 ✅ <b> {} Completed In </b> <code>{}</code>
