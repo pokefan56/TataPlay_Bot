edfa0e25d34b468a3d9aa79ccadd42df60d9e603
pokefan56 <109863157+pokefan56@users.noreply.github.com>
2023-07-18 19:26:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-api_id = 17193413
+api_id = 9976721
 
-api_hash = "5e50e6380252d9d487093bae4171bf91"
+api_hash = "3ef17a8cdb938335bd8ba292e6d816aa"
 
 bot_token = "6094200729:AAF7U2jM_T35Wb4nWdvPdndFNjtZ7oQeTTA"
 
